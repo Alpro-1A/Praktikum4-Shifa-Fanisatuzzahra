d07407e0000e7e7fd5ededfd531a72ec48b04339
shifa <shifanisatuz092GMAIL.com>
2023-11-04 19:00:08 +0700

    Mengumpulkan tugas 4 nomor 2

---
+++ Nomor2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   cout << "   PROGRAM PIRAMIDA BINTANG    " << endl;
+    cout << "-------------------------------" << endl;
+    int s;
+
+    cout << "   Masukkan tinggi piramida : ";
+    cin >> s;
+
+    char currentChar = '*';
+    for (int i = 0; i <= s; i++){
+        for (int j = s; j > i; j--){
+            cout << " ";
+        }
+    for (int k = 1; k <= i; k++){
+        cout << currentChar << " ";
+        }
+
+        cout << endl;
+        }
+    cout << "-------------------------------\n";
+
+    return 0;
+}
